552bd44089f34feecb9895949f44cfa3ec6fb2bf
Shavali388 <dudekulas54@gmail.com>
2025-07-15 12:29:24 +0530

    Update Adding2.java

--- Adding2.java
+++ Adding2.java
@@ -7,6 +7,7 @@ class Adding2
 	public static vod main(String[] args)
 	{
 		System.out.println("This is the second program enterd from gitbash");
+		System.out.println("This is the second program enterd from github");
 	}
 }
 
